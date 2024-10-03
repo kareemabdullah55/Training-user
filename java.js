a4d473988a211e06e08d9a8b959f9c8ec711a4cc
kareemabdullah55 <kareem.abuullah@gmail.com>
2024-10-03 17:59:43 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,114 @@
+const pdfData = [
+  //المناديب //
+    { name: 'SMR01', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR02', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR03', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR06', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR07', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR08', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR09', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR21', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR22', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMR23', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMS01', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMS02', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'SMW01', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR01', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR02', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR03', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR04', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR05', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR16', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR11', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMR18', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMS04', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMW02', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMW01', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMS01', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMS02', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMS03', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    { name: 'CSMS07', url: 'https://reginasalesbuzz.github.io/salesbuzz/Sales%20man.html' },
+    ////ــــــــــــــــ///////////////ــــــــــــــــــــــــ
+//المشرفين//
+    { name: 'm.sabry', url: 'file:///D:/web%20site%20regina/supervisor.html'},
+    { name: 'ahmed.gabr', url: 'file:///D:/web%20site%20regina/supervisor.html'},
+    { name: 'm.abdelrahman', url: 'file:///D:/web%20site%20regina/supervisor.html'},
+    { name: 'ahmed.mostafa', url: 'file:///D:/web%20site%20regina/supervisor.html'},
+    { name: 'm.gaballa', url: 'file:///D:/web%20site%20regina/supervisor.html'},
+    { name: 'm.saltouh', url: 'file:///D:/web%20site%20regina/supervisor.html'},
+    ////ــــــــــــــــ///////////////ــــــــــــــــــــــــ
+//المحاسبين//
+{ name: 'bassem.ramadan', url: 'https://reginasalesbuzz.github.io/salesbuzz/acc1.html' },
+{ name: 'mohamed.magdy', url: 'https://reginasalesbuzz.github.io/salesbuzz/acc1.html' },
+{ name: 'omar.mohamed', url: 'https://reginasalesbuzz.github.io/salesbuzz/acc1.html' },
+{ name: 'saber.mansour', url: 'https://reginasalesbuzz.github.io/salesbuzz/acc1.html' },
+////////////ــــــــــــــــــ////////////ــــــــــــــــــــــ
+//الاداره//
+    { name: 'adham.roshdy' , url: 'https://reginasalesbuzz.github.io/salesbuzz/index.html' },
+    { name: 'amr.helmy' , url: 'https://reginasalesbuzz.github.io/salesbuzz/index.html' },
+    { name: 'bassem.medhat' , url: 'https://reginasalesbuzz.github.io/salesbuzz/index.html' },
+    { name: 'moataz.nabil' , url: 'https://reginasalesbuzz.github.io/salesbuzz/index.html' },
+    { name: 'kareem.abdallah' , url: 'https://reginasalesbuzz.github.io/salesbuzz/index.html' },
+    { name: 'kareem' , url: 'https://reginasalesbuzz.github.io/salesbuzz/index.html' },
+    { name: 'sh.ameen', url: 'https://reginasalesbuzz.github.io/salesbuzz/report.html' },
+//ـــــــــــــــ//ــــــــــــــــــــــــ//ــــــــــــــــ
+//ادارة المبيعات //
+    { name: 'm.elokdy', url: 'https://reginasalesbuzz.github.io/salesbuzz/report.html' },
+    { name: 'montaser.adel', url: 'https://reginasalesbuzz.github.io/salesbuzz/report.html' },
+    { name: 'eid.nagy', url: 'https://reginasalesbuzz.github.io/salesbuzz/report.html' },
+//ــــــــــــــــــــــــــــــ///ـــــــــــــــــــــــ
+ 
+  ];
+    function generateTableRows() {
+      const tableBody = document.getElementById('table-body');
+      tableBody.innerHTML = ''; // Clear any existing rows
+      pdfData.forEach(pdf => {
+        const row = document.createElement('tr');
+        row.classList.add('data-row');
+        row.innerHTML = `
+          <td>${pdf.name}</td>
+          <td><a href="${pdf.url}" target="_blank">اضغط للدخول</a></td>
+        `;
+        tableBody.appendChild(row);
+      });
+    }
+
+    function searchTable() {
+      const searchBar = document.getElementById('search-bar');
+      const searchTerm = searchBar.value.toLowerCase();
+      const table = document.getElementById('data-table');
+      const rows = document.querySelectorAll('.data-row');
+      const noResult = document.getElementById('no-result');
+      let recordFound = false;
+
+      rows.forEach(row => {
+        const cell = row.getElementsByTagName('td')[0];
+        if (cell.textContent.toLowerCase() === searchTerm) {
+          row.classList.remove('hidden');
+          recordFound = true;
+        } else {
+          row.classList.add('hidden');
+        }
+      });
+// here we check on value user enter if it empty condition will show if not result will show
+      if(searchTerm === ''){
+        table.style.display = 'none';
+        noResult.style.display = 'block';
+        noResult.textContent = 'يرجى التواصل مع الدعم الفني'}
+// recordFound mean user already enter value so table will show and <p> tag still hidden 
+// note: recordFound => recordFound==true ----- same are true write this or this same result
+      else if (recordFound) {
+        table.style.display = 'table';
+        noResult.style.display = 'none';
+      } 
+   // here we said to compiler if any above code not exist show message and keep table hidden   
+      else {
+        table.style.display = 'none';
+        noResult.style.display = 'block';
+        noResult.textContent = "يرجى التاكد من كلمة المرور"
+      }
+    }
+// try now reload page first
+//Go
+    // Generate table rows on page load
+    window.onload = generateTableRows;
